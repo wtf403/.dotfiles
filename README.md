0dbe1f744862638fe88fbd0707be3fadd608621a
wtf403 <wtf403@yandex.ru>
2023-03-27 23:55:01 +0300

    update .dotfiles

---
+++ README.md
@@ -0,0 +1,2 @@
+# My MacOS setup
+
---
+++ copy-config.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+## karabiner 
+cp -a /Users/maxkiselev/.config/karabiner /Users/maxkiselev/Desktop/dev/dots
+
+## VScodium
+cp /Users/maxkiselev/Library/Application Support/VSCodium - Insiders/User/keybindings.json /Users/maxkiselev/Desktop/dev/dots
