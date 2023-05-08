ae9217cc3509ac2797c7a0b4730622570fafd715
wtf403 <wtf403@yandex.ru>
2023-05-08 16:15:00 +0300

    cron

--- yabai/.config/yabai/moveDownAndFollowFocus.sh
+++ yabai/.config/yabai/moveDownAndFollowFocus.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-curWindowId="$(yabai -m query --windows --window | jq -re ".id")"
+curWindowId="$(yabai -m query --spaces --window | jq -re ".id")"
 
 $(yabai -m window --swap south || yabai -m window --display south || yabai -m window --swap north || yabai -m window --display north)
 $(yabai -m window --focus "$curWindowId")
--- yabai/.config/yabai/moveLeftAndFollowFocus.sh
+++ yabai/.config/yabai/moveLeftAndFollowFocus.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 
-curWindowId="$(yabai -m query --windows --window | jq -re ".id")"
-
-terminal-notifier -message $(yabai -m query --windows --window | jq -re ".id")
+curWindowId="$(yabai -m query --space --window | jq -re ".id")"
 
 $(yabai -m window --swap west || yabai -m window --display west || yabai -m window --swap east || yabai -m window --display east)
 $(yabai -m window --focus "$curWindowId")
-
-terminal-notifier -message $(yabai -m query --windows --window | jq -re ".id")
-
-
