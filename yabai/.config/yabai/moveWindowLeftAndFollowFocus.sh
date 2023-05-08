cd1d5df84b994090d4423bd041837432cca1bd76
wtf403 <wtf403@yandex.ru>
2023-05-08 15:00:00 +0300

    cron

--- yabai/.config/yabai/moveWindowLeftAndFollowFocus.sh
+++ yabai/.config/yabai/moveWindowLeftAndFollowFocus.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 
 curWindowId="$(yabai -m query --windows --window | jq -re ".id")"
-curLayout="$(yabai -m query --spaces --space | jq -re ".type")"
 
-if [[ "$curLayout" == "stack" ]]; then
-  yabai -m window --focus next
-else
-  yabai -m window --display next || yabai -m window --display first
-  yabai -m window --focus "$curWindowId"
-fi
+$(yabai -m window --display next || yabai -m window --display first)
+$(yabai -m window --focus "$curWindowId")
--- yabai/.config/yabai/moveWindowRightAndFollowFocus.sh
+++ yabai/.config/yabai/moveWindowRightAndFollowFocus.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
 curWindowId="$(yabai -m query --windows --window | jq -re ".id")"
-curSpaceLayout="$(yabai -m query --spaces --space | jq -re ".type")"
 
-if [ "$curSpaceLayout" != "stack" ]; then
-  $(yabai -m window --display next || yabai -m window --display first)
-fi
-
+$(yabai -m window --display next || yabai -m window --display first)
 $(yabai -m window --focus "$curWindowId")
