6a370a1d93851fb65d36024434c788814959e551
wtf403 <wtf403@yandex.ru>
2023-05-09 00:00:00 +0300

    cron

--- yabai/.config/yabai/moveLeft.sh
+++ yabai/.config/yabai/moveLeft.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 
-curWindowId="$(yabai -m query --windows --window | jq -re ".id")"
+WIN_ID=$(yabai -m query --windows --window | jq '.id')
+CUR_DISPLAY=$(yabai -m query --windows --window | jq '.display')
+LEFT_WIN_ID=$(yabai -m query --windows --display $CUR_DISPLAY | jq 'map(select(.frame.x < .frame.x)) | .[-1].id')
+
+if [ -z "$LEFT_WIN_ID" ]; then
+  PREV_DISPLAY=$(yabai -m query --displays --display | jq '.index - 1')
+  LEFT_WIN_ID=$(yabai -m query --windows --display $PREV_DISPLAY | jq 'min_by(.frame.x) | .id')
+fi
+
+# Perform any action on the left window using $LEFT_WIN_ID
+
+terminal-notifier -message "$LEFT_WIN_ID"
 
 $(yabai -m window --focus west || yabai -m display --focus west || yabai -m window --focus east || yabai -m display --focus east)
 $(yabai -m window --focus "$curWindowId")
