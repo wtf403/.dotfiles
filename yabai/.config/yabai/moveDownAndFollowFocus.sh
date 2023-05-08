9add0762e272c05afb75640c3ff31e8d525f4301
wtf403 <wtf403@yandex.ru>
2023-05-08 16:30:00 +0300

    cron

--- yabai/.config/yabai/moveDownAndFollowFocus.sh
+++ yabai/.config/yabai/moveDownAndFollowFocus.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 
-curWindowId="$(yabai -m query --spaces --window | jq -re ".id")"
+curWindowId="$(yabai -m query --windows --window | jq -re ".id")"
 
-$(yabai -m window --swap south || yabai -m window --display south || yabai -m window --swap north || yabai -m window --display north)
-$(yabai -m window --focus "$curWindowId")
+# Swap the window and store the resulting space id
+targetSpaceId=$(
+  yabai -m window --swap south || \
+  yabai -m window --display south || \
+  yabai -m window --swap north || \
+  yabai -m window --display north \
+  | grep -oP '(?<=space )\d+'
+)
+
+# Focus on the target space
+yabai -m space --focus "$targetSpaceId"
+
+# Focus on the original window
+yabai -m window --focus "$curWindowId"
