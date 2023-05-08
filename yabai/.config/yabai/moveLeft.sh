a76f5c819f320501d4232a9b85ade21dab484764
wtf403 <wtf403@yandex.ru>
2023-05-08 23:45:00 +0300

    cron

--- yabai/.config/yabai/moveLeft.sh
+++ yabai/.config/yabai/moveLeft.sh
@@ -2,5 +2,5 @@
 
 curWindowId="$(yabai -m query --windows --window | jq -re ".id")"
 
-$(yabai -m window --swap west || yabai -m window --display west || yabai -m window --swap east || yabai -m window --display east)
+$(yabai -m window --focus west || yabai -m display --focus west || yabai -m window --focus east || yabai -m display --focus east)
 $(yabai -m window --focus "$curWindowId")
