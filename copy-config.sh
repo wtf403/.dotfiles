b3b42413c80c4b74e207fb7f47133ed12d89c74d
wtf403 <wtf403@yandex.ru>
2023-05-11 00:45:04 +0300

    cron

--- copy-config.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-## karabiner 
-cp -a /Users/maxkiselev/.config/karabiner /Users/maxkiselev/Desktop/dev/dots
-
-## VScodium
-cp /Users/maxkiselev/Library/Application Support/VSCodium - Insiders/User/keybindings.json /Users/maxkiselev/Desktop/dev/dots
