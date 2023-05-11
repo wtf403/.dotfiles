3d00e148eeace98419d59d6b69fd59de74406fcb
wtf403 <wtf403@yandex.ru>
2023-05-12 01:45:00 +0300

    cron

--- bin/bin/macos-defaults/init-defaults.sh
+++ bin/bin/macos-defaults/init-defaults.sh
@@ -483,3 +483,8 @@ echo "Done. Note that some of these changes require a logout/restart to take eff
 sudo defaults delete /Library/Preferences/com.apple.airport.bt.plist bluetoothCoexMgmt
 defaults write /Library/Preferences/com.apple.airport.bt.plist bluetoothCoexMgmt Hybrid
 
+# NoTunes replacement Music.app with open.spotify.com
+defaults write digital.twisted.noTunes replacement /Users/maxkiselev/bin/macos-defaults/spotify.webloc
+
+# Change hammerspoon config path
+defaults write org.hammerspoon.Hammerspoon MJConfigFile "~/.config/hammerspoon/init.lua"
