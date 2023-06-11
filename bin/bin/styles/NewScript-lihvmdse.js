e8cda6f19522a5a91e4572e73a8bb7412370b230
wtf403 <wtf403@yandex.ru>
2023-06-11 22:30:01 +0300

    cron

--- bin/bin/styles/NewScript-lihvmdse.js
+++
@@ -1,6 +0,0 @@
-// ==UserScript==
-// @name        NewScript-lihvmdse
-// @description This is your new file, start writing code
-// @match       *://*/*
-// ==/UserScript==
-document.documentElement.setAttribute("data-location", window.location.href);
