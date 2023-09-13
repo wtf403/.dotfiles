14fcc2b9918c8f2c21612940ba922419c83b11ff
wtf403 <wtf403@yandex.ru>
2023-09-13 23:30:02 +0300

    cron

--- yabai/.config/yabai/focus.sh
+++ yabai/.config/yabai/focus.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+# Define variables to store the last known position
+last_x=0
+last_y=0
+
+# Check if the last position is stored in a file
+if [ -f ~/.last_window_position ]; then
+  read -r last_x last_y < ~/.last_window_position
+fi
+
 # Get the focused window's ID
 focused_window_id=$(yabai -m query --windows --window | jq '.id')
 
@@ -13,5 +22,8 @@ height=$(yabai -m query --windows --window | jq '.frame.h')
 center_x=$((x + width / 2))
 center_y=$((y + height / 2))
 
-# Move the mouse cursor to the center of the focused window
-cliclick m:"$center_x","$center_y"
+# Store the current position as the last known position
+echo "$center_x $center_y" > ~/.last_window_position
+
+# Move the mouse cursor to the last known position
+cliclick m:"$last_x","$last_y"
