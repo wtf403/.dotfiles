54fb05f9720fb0171b8856a92fd5abe12b5cc291
wtf403 <wtf403@yandex.ru>
2023-09-15 13:15:01 +0300

    cron

--- yabai/.config/yabai/yabai_configure_space_layouts.sh
+++ yabai/.config/yabai/yabai_configure_space_layouts.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
-
-# Get information about displays using JSON format
 displays=$(yabai -m query --displays)
-
-# Extract the list of spaces on the primary display
 primary_display_spaces=($(echo "$displays" | jq -r '.[0].spaces[]'))
-
-# Extract the list of spaces on the secondary displays
 secondary_display_spaces=($(echo "$displays" | jq -r '.[1:][] | .spaces[]'))
-
-# Configure spaces on the primary display to use the float layout
 for space in "${primary_display_spaces[@]}"; do
   echo $space >> ~/Desktop/.yabai.log
   yabai -m space $space --layout stack
 done
-
-# Configure spaces on the secondary displays to use the bsp layout
 for space in "${secondary_display_spaces[@]}"; do
   echo $space >> ~/Desktop/.yabai.log
   yabai -m space $space --layout bsp
