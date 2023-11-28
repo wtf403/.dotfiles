afcc4c79a85b63afd94b9ae8c2e14e82b71bbfae
wtf403 <wtf403@yandex.ru>
2023-11-28 08:56:07 +0300

    cron

--- macos/.macos/install_dmg.sh
+++ macos/.macos/install_dmg.sh
@@ -1,3 +1,24 @@
 #!/usr/bin/env bash
 
-hdiutil mount $1
+# Check if the correct number of arguments is provided
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 /path/to/your/application.dmg"
+    exit 1
+fi
+
+# Get the DMG file path from the command-line argument
+dmg_path="$1"
+
+# Mount the DMG
+hdiutil attach "$dmg_path"
+
+# Get the name of the mounted volume
+volume_name=$(ls /Volumes/* | grep -m 1 -i ".app")
+
+# Copy the application to the Applications folder
+cp -R "/Volumes/$volume_name" /Applications/
+
+# Eject the DMG
+hdiutil detach "/Volumes/$volume_name"
+
+echo "Installed $volume_name"
